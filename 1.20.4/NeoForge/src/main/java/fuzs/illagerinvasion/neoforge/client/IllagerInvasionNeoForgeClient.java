709a5a00219394e2d4b21d54791533c8664c27ae
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-03-23 22:33:51 +0100

    separate client data gen

--- 1.20.4/NeoForge/src/main/java/fuzs/illagerinvasion/neoforge/client/IllagerInvasionNeoForgeClient.java
+++ 1.20.4/NeoForge/src/main/java/fuzs/illagerinvasion/neoforge/client/IllagerInvasionNeoForgeClient.java
@@ -2,7 +2,12 @@ package fuzs.illagerinvasion.neoforge.client;
 
 import fuzs.illagerinvasion.IllagerInvasion;
 import fuzs.illagerinvasion.client.IllagerInvasionClient;
+import fuzs.illagerinvasion.data.client.ModLanguageProvider;
+import fuzs.illagerinvasion.data.client.ModModelProvider;
+import fuzs.illagerinvasion.neoforge.data.client.ModParticleDescriptionProvider;
+import fuzs.illagerinvasion.neoforge.data.client.ModSpriteSourceProvider;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -14,5 +19,11 @@ public class IllagerInvasionNeoForgeClient {
     @SubscribeEvent
     public static void onConstructMod(final FMLConstructModEvent evt) {
         ClientModConstructor.construct(IllagerInvasion.MOD_ID, IllagerInvasionClient::new);
+        DataProviderHelper.registerDataProviders(IllagerInvasion.MOD_ID,
+                ModLanguageProvider::new,
+                ModModelProvider::new,
+                ModParticleDescriptionProvider::new,
+                ModSpriteSourceProvider::new
+        );
     }
 }
