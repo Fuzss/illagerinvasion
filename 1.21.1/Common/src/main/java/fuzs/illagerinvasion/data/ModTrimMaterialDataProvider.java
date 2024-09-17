ed55b455285e7285412a2bc6d64e16df470ace88
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-09-17 17:19:19 +0200

    update for latest puzzles lib

--- 1.21.1/Common/src/main/java/fuzs/illagerinvasion/data/ModTrimMaterialDataProvider.java
+++ 1.21.1/Common/src/main/java/fuzs/illagerinvasion/data/ModTrimMaterialDataProvider.java
@@ -4,17 +4,23 @@ import fuzs.illagerinvasion.init.ModItems;
 import fuzs.illagerinvasion.init.ModRegistry;
 import fuzs.puzzleslib.api.data.v2.AbstractRegistriesDatapackGenerator;
 import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.world.item.armortrim.TrimMaterial;
 
-public class ModTrimMaterialDataProvider extends AbstractRegistriesDatapackGenerator.TrimMaterials {
+public class ModTrimMaterialDataProvider extends AbstractRegistriesDatapackGenerator<TrimMaterial> {
 
     public ModTrimMaterialDataProvider(DataProviderContext context) {
-        super(context);
+        super(Registries.TRIM_MATERIAL, context);
     }
 
     @Override
-    protected void addBootstrap(BootstrapContext<TrimMaterial> bootstapContext) {
-        this.add(ModRegistry.PLATINUM_TRIM_MATERIAL, ModItems.PLATINUM_SHEET_ITEM.value(), 0x527D7C, 0.2F);
+    public void addBootstrap(BootstrapContext<TrimMaterial> context) {
+        registerTrimMaterial(context,
+                ModRegistry.PLATINUM_TRIM_MATERIAL,
+                ModItems.PLATINUM_SHEET_ITEM.value(),
+                0x527D7C,
+                0.2F
+        );
     }
 }
